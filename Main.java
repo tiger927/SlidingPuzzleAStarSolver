9a592ff339e9032ed151447171fcccc565eea55c
CodeTiger <2624183401@qq.com>
2019-08-21 00:54:30 -0400

    Add files via upload
    The two core files that allow the program to run properly

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PriorityQueue<Status> pq = new PriorityQueue<>();
+		
+		Status s = new Status(new int[][]{{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}});
+		//Status s = new Status(new int[][]{{0,1,2},{3,4,5},{6,7,8}});
+		
+		// Change this part to change the number of scrambled times
+		s.scrumble(4000);
+		s.print();
+		pq.add(s);
+		int counter = 0;
+		//HashSet to avoid a same status from being added twice
+		HashSet<Long> hs = new HashSet<Long>();
+		Status fina = new Status(new int[][]{});
+		while(pq.size() != 0) {
+			counter++;
+			Status cur = pq.poll();
+			long tmp = cur.Hash();
+			if(hs.contains(tmp)) continue;
+			hs.add(tmp);
+			if(cur.Calc() == 0) {
+				System.out.println("After iterating through " + counter + " statues, we found the solution in " + cur.scoreG + " steps!");
+				fina = cur;
+				break;
+			}
+			for(int i = 0;i < 4;i++) {
+				Status next = cur.move(i);
+				if(next.scoreG == -1) continue;
+				pq.add(next);
+			}
+		}
+		fina.print();
+
+		
+		
+	}
+
+
+}
---
+++ Status.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+
+public class Status implements Comparable<Status>{
+	public static int[] dx = new int[]{1,0,-1,0};
+	public static int[] dy = new int[]{0,1,0,-1};
+	
+	int[][] arr;
+	int n;
+	int x;
+	int y;
+	int scoreH = -1;
+	int scoreG = 0;
+	
+	public Status(int[][] arr) {
+		this.n = arr.length;
+		this.arr = new int[n][n];
+		for(int i = 0;i < n;i++) {
+			for(int j = 0;j < n;j++) {
+				if(arr[i][j] == 0) {
+					this.x = i;
+					this.y = j;
+				}
+				this.arr[i][j] = arr[i][j];
+			}
+		}
+	}
+	
+	public long Hash() {
+		long res = 0;
+		for(int i = 0;i < this.n;i++) {
+			for(int j = 0;j < this.n;j++) {
+				res += arr[i][j] * Math.pow(3,j + i * this.n);
+			}
+		}
+		return res;
+	}
+	
+	
+	public int Calc() {
+		if(this.scoreH != -1) return scoreH;
+		int res = 0;
+		for(int i = 0;i < this.n;i++) {
+			for(int j = 0;j < this.n;j++) {
+				int shouldX = arr[i][j] / this.n;
+				int shouldY = arr[i][j] % this.n;
+				res += Math.abs(shouldX - i) + Math.abs(shouldY - j);
+			}
+		}
+		this.scoreH = res;
+		return res;
+	}
+	
+	public Status move(int d) {
+		Status res = new Status(this.arr);
+		int newX = this.x + dx[d];
+		int newY = this.y + dy[d];
+		if(newX < 0 || newX >= this.n || newY < 0 || newY >= this.n) {
+			res.scoreG = -1;
+			return res;
+		}
+		res.arr[this.x][this.y] = res.arr[newX][newY];
+		res.arr[newX][newY] = 0;
+		res.x = newX;
+		res.y = newY;
+		res.Calc();
+		res.scoreG = this.scoreG + 1;
+		return res;
+	}
+
+	@Override
+	public int compareTo(Status o) {
+		//It seems that changing this formula can greatly help the performance of this algorithm
+		return (2 * this.Calc() + this.scoreG - 2 * o.Calc() - o.scoreG);
+	}
+	
+	
+	public void print() {
+		for(int i = 0;i < this.n;i++) {
+			System.out.println(Arrays.toString(this.arr[i]));
+		}
+		System.out.println();
+	}
+	
+	public void scrumble(int k) {
+		if(k < 0) return;
+		int d = (int)(Math.random() * 4);
+		int newX = this.x + dx[d];
+		int newY = this.y + dy[d];
+		if(newX < 0 || newX >= this.n || newY < 0 || newY >= this.n) {
+			scrumble(k);
+		}else{
+			arr[this.x][this.y] = arr[newX][newY];
+			arr[newX][newY] = 0;
+			this.x = newX;
+			this.y = newY;
+			scrumble(k - 1);
+		}
+	}
+}
