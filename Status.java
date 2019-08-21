1cd0af281e96b1bf4a3d2698094cd8520f5c7864
CodeTiger <2624183401@qq.com>
2019-08-21 01:44:49 -0400

    Update Status.java
    4 instead of 2 seems to work much better when solving 5 * 5 puzzles

--- Status.java
+++ Status.java
@@ -70,7 +70,7 @@ public class Status implements Comparable<Status>{
 	@Override
 	public int compareTo(Status o) {
 		//It seems that changing this formula can greatly help the performance of this algorithm
-		return (2 * this.Calc() + this.scoreG - 2 * o.Calc() - o.scoreG);
+		return (4 * this.Calc() + this.scoreG - 4 * o.Calc() - o.scoreG);
 	}
 	
 	
